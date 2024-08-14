dde6407289b14b0acfb7ef9807a40651cd54c324
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 15:11:39 +0200

    rpl

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  assetPrefix: "/rpl.rubenportfolio/",
+  basePath: "/rpl.rubenportfolio",
   trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
