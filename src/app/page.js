2543299588550f4b7eaf4b3a69b6b6946d91b972
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-15 00:37:55 +0200

    rpl

--- src/app/page.js
+++ src/app/page.js
@@ -135,7 +135,9 @@ export default function Home() {
           )}
         </PageTransition>
       )}
-      <Footer />
+      <div className="fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center  pt-4  pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
+        © 2024 - Rubén Pérez Lara
+      </div>
     </div>
   );
 }
