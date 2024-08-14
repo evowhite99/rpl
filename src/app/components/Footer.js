60070ab7e3a23fb288eca0484831cd6621b20355
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 23:56:20 +0200

    rpl

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,8 @@
+"use client";
+
 export default function Footer() {
   return (
-    <div className="fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center lg:pt-8  pt-4 lg:pb-8 pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
+    <div className="mx-auto my-auto fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center lg:pt-8  pt-4 lg:pb-8 pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
       © 2024 - Rubén Pérez Lara
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,9 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={`${inter.className}  `}>
+        <Footer />
         <PreventZoom />
         <div className="">{children}</div>
-        <Footer />
       </body>
     </html>
   );
