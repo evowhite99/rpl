c444c92b21aa45718b524a271420fa012a31d892
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 15:28:15 +0200

    rpl

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix: "/rpl.rubenportfolio/",
-  basePath: "/rpl.rubenportfolio",
-  trailingSlash: true,
-  output: "export",
-};
+const nextConfig = {};
 
 export default nextConfig;
