172c74a1b489bbef57f06a44e81e5704ccd2c84d
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 23:06:47 +0200

    rpl

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,8 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${inter.className}  `}>
         <PreventZoom />
-        <Footer />
         <div className="">{children}</div>
+        <Footer />
       </body>
     </html>
   );
