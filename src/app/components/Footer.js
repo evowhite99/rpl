d51f668a06429e9dfa4cd977985c4bc7f62497ed
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 22:53:11 +0200

    rpl

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+
 export default function Footer() {
+  useEffect(() => {
+    // Esto forzará un repaint del footer en la carga de la página.
+    const footer = document.getElementById("footer");
+    footer.style.display = "none";
+    footer.offsetHeight; // Forzar reflow
+    footer.style.display = "block";
+  }, []);
+
   return (
-    <div className="fixed block inset-x-0 bottom-0 z-10 text-center justify-center items-center lg:pt-8  pt-4 lg:pb-8 pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
+    <div
+      id="footer"
+      className="fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center lg:pt-8 pt-4 lg:pb-8 pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%"
+    >
       © 2024 - Rubén Pérez Lara
     </div>
   );
