56244f75703086c5101d50a2e7bee44a666d5f78
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 17:39:33 +0200

    rpl

--- src/app/page.js
+++ src/app/page.js
@@ -26,6 +26,7 @@ function ResponsiveCamera({ isMobile }) {
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
   const [page, setPage] = useState(0); // Estado que controla la página actual
+  const [loading, setLoading] = useState(true); // Estado de carga del modelo
 
   useEffect(() => {
     const handleResize = () => {
@@ -35,6 +36,9 @@ export default function Home() {
     window.addEventListener("resize", handleResize);
     handleResize(); // Ejecutar una vez al montar el componente
 
+    // Simulación de carga de modelo 3D (ejemplo: 2 segundos)
+    const timeout = setTimeout(() => setLoading(false), 700);
+
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
@@ -42,91 +46,96 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-screen bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90% overflow-x-hidden">
-      <PageTransition page={page}>
-        {page === 0 && (
-          <div key="home" className="w-screen h-screen relative">
-            {/* El modelo 3D estará al fondo */}
-            <Canvas className="fixed inset-0 z-0">
-              <ResponsiveCamera isMobile={isMobile} />
-              <ambientLight intensity={0.7} />
-              <directionalLight position={[10, 10, 5]} intensidad={3} />
-              <pointLight position={[0, 10, 10]} intensidad={1} />
-              <Environment preset="forest" />
-              <Suspense fallback={<LoadingSpinner />}>
-                <Model scale={0.5} position={modelPosition} />
-              </Suspense>
-              <OrbitControls
-                enableZoom={false}
-                enableRotate={false}
-                enablePan={false}
-              />
-            </Canvas>
+      {loading ? (
+        <div className="fixed inset-0 z-10 flex justify-center items-center ">
+          <p className="text-2xl font-bold">Loading...</p>
+        </div>
+      ) : (
+        <PageTransition page={page}>
+          {page === 0 && (
+            <div key="home" className="w-screen h-screen relative">
+              {/* Mostrar un indicador de carga mientras el modelo se carga */}
 
-            {/* Contenedor de botones centrado verticalmente a la izquierda */}
-            <div className="fixed inset-x-0 bottom-0 lg:bottom-0 top-0 lg:top-auto z-10 flex flex-col justify-center items-center lg:justify-end lg:pb-32 pb-52 pt-60 lg:pt-0">
-              {/* Lista de botones */}
-              <div className="grid lg:grid-cols-4 grid-cols-1 gap-8">
-                <button
-                  onClick={() => setPage(1)}
-                  className="bg-blue-500 text-white py-4 px-20 rounded text-center  hover:bg-green-500 hover:scale-110 duration-300"
-                  style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
-                >
-                  Música
-                </button>
-                <button
-                  onClick={() => setPage(2)}
-                  className="bg-blue-500 text-white py-4 px-20 rounded text-center hover:bg-green-500 hover:scale-110 duration-300"
-                  style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
-                >
-                  Videojuegos
-                </button>
-                <a
-                  href="https://www.rubenportfolio.com/"
-                  className="bg-blue-500 text-white py-4 px-20 rounded text-center hover:bg-green-500 hover:scale-110 duration-300"
-                  style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
-                >
-                  Portfolio
-                </a>
-                <button
-                  className="bg-blue-500 text-white text-center py-4 px-20 rounded hover:bg-green-500 hover:scale-110 duration-300"
-                  style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
-                >
-                  Próximamente...
-                </button>
+              <Canvas className="fixed inset-0 z-0">
+                <ResponsiveCamera isMobile={isMobile} />
+                <ambientLight intensity={0.7} />
+                <directionalLight position={[10, 10, 5]} intensidad={3} />
+                <pointLight position={[0, 10, 10]} intensidad={1} />
+                <Environment preset="city" />
+                <Model scale={0.5} position={modelPosition} />
+                <OrbitControls
+                  enableZoom={false}
+                  enableRotate={false}
+                  enablePan={false}
+                />
+              </Canvas>
+
+              {/* Contenedor de botones centrado verticalmente a la izquierda */}
+              <div className="fixed inset-x-0 bottom-0 lg:bottom-0 top-0 lg:top-auto z-10 flex flex-col justify-center items-center lg:justify-end lg:pb-32 pb-52 pt-60 lg:pt-0">
+                {/* Lista de botones */}
+                <div className="grid lg:grid-cols-4 grid-cols-1 gap-8">
+                  <button
+                    onClick={() => setPage(1)}
+                    className="bg-blue-500 text-white py-4 px-20 rounded text-center  hover:bg-green-500 hover:scale-110 duration-300"
+                    style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
+                  >
+                    Música
+                  </button>
+                  <button
+                    onClick={() => setPage(2)}
+                    className="bg-blue-500 text-white py-4 px-20 rounded text-center hover:bg-green-500 hover:scale-110 duration-300"
+                    style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
+                  >
+                    Videojuegos
+                  </button>
+                  <a
+                    href="https://www.rubenportfolio.com/"
+                    className="bg-blue-500 text-white py-4 px-20 rounded text-center hover:bg-green-500 hover:scale-110 duration-300"
+                    style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
+                  >
+                    Portfolio
+                  </a>
+                  <button
+                    className="bg-blue-500 text-white text-center py-4 px-20 rounded hover:bg-green-500 hover:scale-110 duration-300"
+                    style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)" }}
+                  >
+                    Próximamente...
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {page === 1 && (
-          <div key="music" className="w-screen min-h-screen relative">
-            {/* Aquí iría la sección de música */}
-            <button
-              className="hover:bg-red-700"
-              onClick={() => setPage(0)}
-              style={{
-                position: "fixed",
-                top: "20px",
-                left: "20px",
-                padding: "10px 20px",
-                cursor: "pointer",
-                border: "none",
-                borderRadius: "5px",
-                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)",
-                zIndex: 10,
-              }}
-            >
-              Volver
-            </button>
-          </div>
-        )}
+          {page === 1 && (
+            <div key="music" className="w-screen min-h-screen relative">
+              {/* Aquí iría la sección de música */}
+              <button
+                className="hover:bg-red-700"
+                onClick={() => setPage(0)}
+                style={{
+                  position: "fixed",
+                  top: "20px",
+                  left: "20px",
+                  padding: "10px 20px",
+                  cursor: "pointer",
+                  border: "none",
+                  borderRadius: "5px",
+                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)",
+                  zIndex: 10,
+                }}
+              >
+                Volver
+              </button>
+            </div>
+          )}
 
-        {page === 2 && (
-          <div key="games" className="w-screen min-h-screen relative">
-            <VideojuegosSection onBack={() => setPage(0)} />
-          </div>
-        )}
-      </PageTransition>
+          {page === 2 && (
+            <div key="games" className="w-screen min-h-screen relative">
+              <VideojuegosSection onBack={() => setPage(0)} />
+            </div>
+          )}
+        </PageTransition>
+      )}
     </div>
   );
 }
