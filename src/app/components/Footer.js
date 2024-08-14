e1674c7833f3cf039f850cee76a70bc06e9a0d7a
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-14 23:33:50 +0200

    rpl

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,4 +1,3 @@
-"use client";
 export default function Footer() {
   return (
     <div className="fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center lg:pt-8  pt-4 lg:pb-8 pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
