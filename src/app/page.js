4d0c0b26d2e05c10312d6b797f8e80821cff80c6
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-15 00:51:20 +0200

    rpl

--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen  w-screen bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90% ">
-      <div className="fixed inset-x-0 bottom-0 z-10 text-center justify-center items-center  pt-4  pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
+      <div className="fixed inset-x-0 bottom-0 z-50 text-center justify-center items-center  pt-4  pb-6 bg-gradient-to-r from-blue-800 from-10% via-blue-500 via-30% to-blue-950 to-90%">
         © 2024 - Rubén Pérez Lara
       </div>
 
