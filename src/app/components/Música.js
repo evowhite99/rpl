52cd1fcebb6c917e57a7093b9cb0513dc46460e7
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-15 19:17:21 +0200

    rpl

--- src/app/components/Música.js
+++ src/app/components/Música.js
@@ -126,7 +126,11 @@ export default function MusicaSection({ onBack }) {
           controls
           autoPlay
           playsinline
-          preload="auto"
+          poster={
+            currentVideoIndex === 0
+              ? "../videos/thumbnails/db2024-1-thumb.jpg"
+              : null
+          }
           className="lg:w-1/5 lg:h-1/5 w-3/5 h-3/5 rounded-lg"
         />
 
