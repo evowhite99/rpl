9d86fbd654e340f057d03b3d0fbc803ea469d7a3
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-15 14:15:59 +0200

    rpl

--- src/app/components/Videojuegos.js
+++ src/app/components/Videojuegos.js
@@ -1,43 +1,77 @@
+import { useState } from "react";
+
 export default function VideojuegosSection({ onBack }) {
   // Desestructurando la prop onBack
   const gamesCol = [
-    { name: "Demon's Souls" },
-    { name: "Ender Lilies: Quietus of the Knights" },
-    { name: "Doom Eternal" },
-    { name: "Need For Speed Underground 2" },
-    { name: "Final Fantasy XVI" },
-    { name: "Burnout Paradise" },
-    { name: "Gran Turismo 7" },
-    { name: "Wipeout Omega Collection" },
-    { name: "Grand Theft Auto Vice City" },
-    { name: "Bloodstained Ritual of the Night" },
-    { name: "Gran Turismo 3" },
-    { name: "Need For Speed Hot Pursuit" },
-    { name: "Rayman" },
-    { name: "Nier Replicant ver.1.22474487139..." },
-    { name: "Tomba! 2" },
-    { name: "Drakengard 3" },
-    { name: "Hotline Miami 2 Wrong Number" },
-    { name: "A hat in time" },
-    { name: "Need For Speed Pro Street" },
-    { name: "Metal Gear Solid 3 Snake Eater" },
-    { name: "Burnout 3 Takedown" },
-    { name: "Castlevania Symphony of the Night" },
-    { name: "Castlevania Dawn of Sorrow" },
-    { name: "Oddworld Soulstorm" },
-    { name: "Dreams PS4" },
-    { name: "Jak X" },
-    { name: "Wreckfest" },
-    { name: "Ratchet & Clank 3 Up your Arsenal" },
-    { name: "Resistance 3" },
-    { name: "Shadow of the Tomb Raider" },
+    { name: "Demon's Souls", image: "../images/demonssouls.jpg" },
+    {
+      name: "Ender Lilies: Quietus of the Knights",
+      image: "../images/enderlilies.jpg",
+    },
+    { name: "Doom Eternal", image: "../images/doometernal.jpg" },
+
+    { name: "Final Fantasy XVI", image: "../images/ffxvi.jpg" },
+    { name: "Burnout Paradise", image: "../images/burnoutparadise.jpg" },
+    { name: "Gran Turismo 7", image: "../images/granturismo7.jpg" },
+    { name: "Wipeout Omega Collection", image: "../images/wipeout.jpg" },
+
+    {
+      name: "Bloodstained Ritual of the Night",
+      image: "../images/bloodstained.jpg",
+    },
+
+    {
+      name: "Need For Speed Hot Pursuit",
+      image: "../images/nfshotpursuit.jpg",
+    },
+    { name: "Rayman", image: "../images/rayman.jpg" },
+    {
+      name: "Nier Replicant ver.1.22474487139...",
+      image: "../images/nierreplicant.jpg",
+    },
+    { name: "Tomba! 2", image: "../images/tomba2!.png" },
+    { name: "Drakengard 3", image: "../images/drakengard3.jpg" },
+    {
+      name: "Hotline Miami 1 + 2 Wrong Number",
+      image: "../images/hotlinemiami.jpg",
+    },
+    { name: "A hat in time", image: "../images/ahatintime.jpeg" },
+    { name: "Need For Speed Pro Street", image: "../images/nfsprostreet.jpg" },
+    {
+      name: "Metal Gear Solid 3 Snake Eater",
+      image: "../images/mgs3.webp",
+    },
+    { name: "Burnout 3 Takedown", image: "../images/burnout3.jpg" },
+    {
+      name: "Castlevania Symphony of the Night",
+      image: "../images/castlevaniasotn.webp",
+    },
+    {
+      name: "Castlevania Dawn of Sorrow",
+      image: "../images/castlevaniados.jpg",
+    },
+    { name: "Oddworld Soulstorm", image: "../images/oddworld.jpg" },
+    { name: "Dreams PS4", image: "../images/dreams.jpg" },
+    { name: "Jak X", image: "../images/jakx.jpg" },
+    { name: "Wreckfest", image: "../images/wreckfest.jpg" },
+    {
+      name: "Ratchet & Clank 3 Up your Arsenal",
+      image: "../images/ratchet3.jpg",
+    },
   ];
 
+  // Estado para controlar cuántas imágenes mostrar
+  const [visibleGames, setVisibleGames] = useState(10);
+
+  const handleLoadMore = () => {
+    setVisibleGames((prevVisibleGames) => prevVisibleGames + 10);
+  };
+
   return (
-    <div className="text-lg text-center  lg:pb-32">
+    <div className="text-lg text-center lg:pb-32">
       <button
         className="hover:bg-red-700 fixed left-0 ml-52 mt-3"
-        onClick={onBack} // Aquí se llama correctamente a la función onBack
+        onClick={onBack}
         style={{
           padding: "10px 20px",
           cursor: "pointer",
@@ -49,21 +83,31 @@ export default function VideojuegosSection({ onBack }) {
       >
         Volver
       </button>
-      <div className=" text-2xl font-extrabold italic pt-5">
+      <div className="text-2xl font-extrabold italic pt-5">
         Videojuegos recomendados
       </div>
-      <div className="flex flex-col min-h-screen justify-between">
-        <div className="mt-10 grid lg:grid-cols-2 grid-cols-2 text-left bg-cyan-900 bg-opacity-30 lg:w-7/12 w-11/12 mx-auto pl-10 rounded-3xl pb-5">
-          {gamesCol.map((game) => (
-            <div
-              key={game.name}
-              className="mt-5 lg:pl-5 lg:pr-5 font-lilita outlineHeader lg:text-xl text-sm  text-yellow-400 dark:text-blue-500 lg:w-auto w-11/12 flex justify-start items-start"
-            >
-              <span className="text-white">{game.name}</span>
-            </div>
-          ))}
-        </div>
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 text-left bg-cyan-900 bg-opacity-30 lg:w-7/12 w-11/12 mx-auto p-3 rounded-3xl pb-5">
+        {gamesCol.slice(0, visibleGames).map((game) => (
+          <div key={game.name} className="relative">
+            <img
+              src={game.image}
+              alt={game.name}
+              className="w-full h-auto rounded-lg hover:scale-105 transition-transform duration-300"
+            />
+            <span className="absolute bottom-0 left-0 bg-opacity-75 bg-black text-white text-sm p-2 rounded-t-lg">
+              {game.name}
+            </span>
+          </div>
+        ))}
       </div>
+      {visibleGames < gamesCol.length && (
+        <button
+          onClick={handleLoadMore}
+          className="mt-10 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-green-500 duration-300"
+        >
+          Cargar más
+        </button>
+      )}
     </div>
   );
 }
