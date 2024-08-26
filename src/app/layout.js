becea11600df4588df580ffd8175a0dec1d68ebd
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-26 16:56:17 +0200

    rpl

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="images/favicon.ico" />
       </head>
       <body className={`${inter.className}  `}>
         <PreventZoom />
